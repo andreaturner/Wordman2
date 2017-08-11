c3d2220dd5a7ed64cfaff418fc580f526f5af926
Paige Israel <paigeaisrael@gmail.com>
2017-08-11 05:39:32 -0700

    Changed class name in about.php
    1. Changed class name to be mroe descriptive
    2. Starting work on essays-collecitons.php

--- pages/essays-collections.php
+++ pages/essays-collections.php
@@ -4,7 +4,7 @@
 ?>
 
     <div class="books-page">
-        <div class="keep-it-together">
+        <div class="overlow-auto">
             
             <div class="header-novel">
                 <a href="under-construction.php"><span>E</span>ssays & <span>C</span>ollections</a>
@@ -13,7 +13,7 @@
                 <a href="under-construction.php"><img src="../images/book-tree-enter-647x800.jpg" width="647" height="800" alt="Essays and Collections Image" /></a>
             </div>
             
-        </div>  <!-- end tkeep-it-together div -->
+        </div>  <!-- end overlow-auto div -->
     </div> <!-- [end books-page div] -->       
 <?php
     include ('../shared/footer.html');            

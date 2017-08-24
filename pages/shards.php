e3069ffd8a8565a43e455d3b73a1347403ee5b57
Paige Israel <paigeaisrael@gmail.com>
2017-08-23 23:37:34 -0700

    Added header to shards.php
    1. Added  header in two lines to shards.php

--- pages/shards.php
+++ pages/shards.php
@@ -6,7 +6,8 @@
 	<div class="page-wdth100pct">
 		<div class="overflow-auto">
 			<div class="header-center-bold">
-                <span>S</span>hards of a <span>B</span>roken <span>M</span>irror
+                <span>S</span>hards of a <span>B</span>roken <span>M</span>irror:<br>
+                <span class="italic smaller">&#40;Songs&#44; Poems&#44; &#38; Thoughts &#47; Journals&#41;</span>
             </div>
             <div class="center">
                 <img src="../images/shards-of-a-broken-mirror-800x492.jpg" width="800" height="492" alt="Shards of a broken mirror image" />

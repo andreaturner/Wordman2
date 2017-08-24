935922ac464dec5eca0cf7fb885ff2e993c48ac8
Paige Israel <paigeaisrael@gmail.com>
2017-08-24 00:22:49 -0700

    Added linkes fot shards.php
    1. Added links to songs-poems-thoughts.php to shards.php
    2. Began coding songs-poems-thoughts.php

--- pages/shards.php
+++ pages/shards.php
@@ -1,17 +1,17 @@
 <?php
-    $page_title = 'Songs&#44; Poems&#44; &#38; Thoughts';
+    $page_title = 'Shards of a Broken Mirror';
     include ('../shared/header.html');
 ?>
 
 	<div class="page-wdth100pct">
 		<div class="overflow-auto">
-		
+
 			<div class="header-center-bold">
-                Shards of a Broken Mirror:<br>
-                <span class="italic smaller">&#40;Songs&#44; Poems&#44; &#38; Thoughts &#47; Journals&#41;</span>
+                <a href="songs-poems-thoughts.php">Shards of a Broken Mirror:</a><br>
+                <a href="songs-poems-thoughts.php"><span class="italic smaller">&#40;Songs&#44; Poems&#44; &#38; Thoughts &#47; Journals&#41;</span></a>
             </div>
             <div class="center">
-                <img src="../images/shards-of-a-broken-mirror-800x492.jpg" width="800" height="492" alt="Shards of a broken mirror image" />
+                <a href="songs-poems-thoughts.php"><img src="../images/shards-of-a-broken-mirror-800x492.jpg" width="800" height="492" alt="Shards of a broken mirror image" /></a>
             </div>
             <div class="center">
                 <span class="medium">Created, Composed &#38; Written by <span class="vivaldi medium"><span class="large-130">E</span>ric <span class="large-130">K</span>och</span><span class="italic"> from 1967 through 2017&#46;</span></span>
@@ -28,6 +28,7 @@
             <!-- <div class="credit-bottom-right">
             	Eric Koch
             </div> -->
+	
 
 		</div>  <!-- end overlow-auto div -->
     </div> <!-- [end page-wdth100pct div] -->       
---
+++ pages/songs-poems-thoughts.php
@@ -0,0 +1,20 @@
+<?php
+    $page_title = 'Songs&#44; Poems&#44; &#38; Thoughts';
+    include ('../shared/header.html');
+?>
+
+<div class="page-wdth100pct">
+		<div class="overflow-auto">
+
+			<div class="header-center-bold">
+                Songs&#44; Poems&#44; &#38; Thoughts &#47; Journals&#41;
+            </div>
+            
+	
+
+		</div>  <!-- end overlow-auto div -->
+    </div> <!-- [end page-wdth100pct div] -->       
+
+<?php
+    include ('../shared/footer.html');            
+?>

4d4c31e91cef96e536831ced9042846de0ee950e
Paige Israel <paigeaisrael@gmail.com>
2017-08-23 22:56:31 -0700

    Added image to shards.php
    1. Resized image shards-of-a-broken-mirror.jpg
    2. Added image to shards.php

--- pages/shards.php
+++ pages/shards.php
@@ -2,7 +2,20 @@
     $page_title = 'Songs&#44; Poems&#44; &#38; Thoughts';
     include ('../shared/header.html');
 ?>
+
+	<div class="page-wdth100pct">
+		<div class="overflow-auto">
+			<div class="header-center-bold">
+                <span>S</span>hards of a <span>B</span>roken <span>M</span>irror
+            </div>
+            <div class="center">
+                <img src="../images/shards-of-a-broken-mirror-800x492.jpg" width="800" height="492" alt="Shards of a broken mirror image" />
+            </div>
 	
+
+		</div>  <!-- end overlow-auto div -->
+    </div> <!-- [end page-wdth100pct div] -->       
+
 <?php
     include ('../shared/footer.html');            
 ?>

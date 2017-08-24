3589057a557a53060c8a8dc44996ee45338c40de
Paige Israel <paigeaisrael@gmail.com>
2017-08-24 08:41:03 -0700

    Fixed center on songs-poems-thoughts.php
    1. Created new div with .center class for picture and caption

--- pages/songs-poems-thoughts.php
+++ pages/songs-poems-thoughts.php
@@ -12,24 +12,30 @@
 
             <!-- start the columns of images for the three sections -->
             <div class="col-wdth30pct">
-            	<a href="songs.php"><img src="../images/songs-600x564.jpg" width="600" height="564" alt="Songs image" /></a>
-            	<div class="header-center-bold">
-            		<a href="songs.php">Songs</a>
-            	</div>
+                <div class="center">
+            	   <a href="songs.php"><img src="../images/songs-600x564.jpg" width="600" height="564" alt="Songs image" /></a>
+            	   <div class="header-center-bold">
+            		  <a href="songs.php">Songs</a>
+            	   </div>
+                </div>
             </div>
+
             <div class="col-wdth30pct">
-            	<a href="poems.php"><img src="../images/poems-600x454.jpg" width="600" height="454" alt="Poems image" /></a>
-            	<div class="header-center-bold">
-            		<a href="poems.php">Poems</a>
-            	</div>
+                <div class="center">
+            	   <a href="poems.php"><img src="../images/poems-600x454.jpg" width="600" height="454" alt="Poems image" /></a>
+            	   <div class="header-center-bold">
+            		  <a href="poems.php">Poems</a>
+            	   </div>
+                </div>
             </div>
+
             <div class="col-wdth30pct">
             	<a href="thoughts.php"><img src="../images/thoughts-600x612.jpg" width="600" height="612" alt="Thoughts image" /></a>
             </div>
             
 	
 
-		</div>  <!-- end overlow-auto div -->
+		</div>  <!-- end overflow-auto div -->
     </div> <!-- [end page-wdth100pct div] -->       
 
 <?php

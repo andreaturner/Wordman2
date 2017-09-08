410c470bcbcd4fc830b06c951b820102094a88e6
Paige Israel <paigeaisrael@gmail.com>
2017-09-07 19:18:17 -0700

    Completed hover over on essays-collections.php
    1. Changed image on essays-collecitions.php
    2. Created mouseover and  mouseout on image

--- pages/essays-collections.php
+++ pages/essays-collections.php
@@ -10,7 +10,7 @@
                 <a href="under-construction.php"><span>E</span>ssays &#38; <span>C</span>ollections</a>
             </div>
             <div class="center">
-                <a href="under-construction.php"><img src="../images/book-tree-647x800.jpg" width="647" height="800" onmouseover="this.src='../images/book-tree-enter-647x800.jpg'" onmouseout="this.src='../images/book-tree-647x800.jpg'" alt="Essays and Collections Image" /></a>
+                <a href="under-construction.php"><img src=".. /images/essays-collections-10A-880x644.jpg" width="880" height="644" onmouseover="this.src='../images/essays-collections-10A-880x644-enter.jpg'" onmouseout="this.src='../images/essays-collections-10A-880x644.jpg'" alt="Essays and Collections Image" /></a>
             </div>
             
         </div>  <!-- end overlow-auto div -->

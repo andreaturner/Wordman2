70914195486db8584d99596e7cd7c85a2a193da0
Paige Israel <paigeaisrael@gmail.com>
2017-09-07 17:05:46 -0700

    Starting essays-books.php
    1. Started essays-books.php
    2. Created title
    3. Getting ready for images

---
+++ pages/essays-books.php
@@ -0,0 +1,23 @@
+<?php
+    $page_title = 'Essays &#38; Collections';
+    include ('../shared/header.html');
+?>
+
+	<div class="page-wdth100pct">
+		<div class="overflow-auto">
+
+			<<div class="header-center-bold">
+                <span>E</span>ssays &#38; <span>C</span>ollections</a>
+            </div>
+
+            
+            <!-- <div class="credit-bottom-right">
+            	Eric Koch
+            </div> -->
+	
+		</div>  <!-- end overlow-auto div -->
+    </div> <!-- [end page-wdth100pct div] -->       
+
+<?php
+    include ('../shared/footer.html');            
+?>

b741cb66192e20a2bcc5c85dea6f2bc266c6f8bb
Paige Israel <paigeaisrael@gmail.com>
2017-09-13 05:55:47 -0700

    Started songs.php
    1. Fixed image on essays-books.php
    2. Started and completed songs.php

---
+++ pages/songs.php
@@ -0,0 +1,32 @@
+<?php
+    $page_title = 'Songs';
+    include ('../shared/header.html');
+?>
+
+	<div class="page-wdth100pct">
+
+		<div class="header-center-bold">
+            <span>S</span>ongs
+        </div>
+
+        <div class="small-header-esssay">
+        	&#40;1&#44;140 Songs&#44;Music &#38; Lyrics&#41;
+        </div>
+
+        <div class="center">
+             <img src="../images/songs-500x470.jpg" width="500" height="470" alt="Songs Image" />
+        </div>
+
+        <div class="small-header-esssay">
+        	<a href="57-ford.php">&#39;57 Ford</a><br>
+        	<a href="touch-of-christmas.php">A Small Touch of Christmas</a>
+        </div>
+
+
+
+
+	</div> <!-- End page-wdth100pct div -->
+ 
+<?php
+    include ('../shared/footer.html');            
+?>

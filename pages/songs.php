09bec498b52c49a4573090f2cbe2f09544b39505
Paige Israel <paigeaisrael@gmail.com>
2017-09-18 16:40:51 -0700

    Changed verbage on songs.php
    1. Changed verbage to indicated where to click
    2. Changed name of 57-ford.php
    3. Fixed typo in class name and brokd things. reverted back :(

--- pages/songs.php
+++ pages/songs.php
@@ -9,16 +9,16 @@
             <span>S</span>ongs
         </div>
 
-        <div class="small-header-esssay">
-        	&#40;1&#44;140 Songs&#44;Music &#38; Lyrics&#41;
-        </div>
-
         <div class="center">
              <img src="../images/songs-500x470.jpg" width="500" height="470" alt="Songs Image" />
         </div>
 
-        <div class="small-header-esssay">
-        	<a href="57-ford.php">&#39;57 Ford</a><br>
+        <div class="small-header-essay">
+            1&#44;140 Songs&#44; Music &#38; Lyrics<br>
+            Read some samples here!
+        </div>
+        <div class="small-header-essay">
+       	    <a href="fifty-seven-ford.php">&#39;57 Ford</a><br>
         	<a href="touch-of-christmas.php">A Small Touch of Christmas</a>
         </div>
 

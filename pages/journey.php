860313f46d05c44cdbd67dac168e7bdf859c4c95
Paige Israel <paigeaisrael@gmail.com>
2017-09-18 23:09:11 -0700

    Started journey.php
    1. Started work on journey.php

---
+++ pages/journey.php
@@ -0,0 +1,90 @@
+<?php
+    $page_title = 'The Journey Ongoing';
+    include ('../shared/header.html');
+?>
+
+	<div class="page-wdth100pct">
+
+		<div class="header-center-bold">
+            The Journey Ongoing
+        </div>
+
+        <div class="songs center">
+            And when the moon swims &#39;cross the blackest of sky&#44;<br>
+            Oft&#39; then we fail with our very own eye<br>
+            To see the silhouette of souls masked &#39;gainst the light&#44; <br>
+            Taking flight&#8230;<br>
+            Ne&#39;re giving thought to the hearts left behind<br>
+            Searching through bewildered tears&#46;<br><br>
+
+            Is it so strange that it should be ever thus,<br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+
+
+
+
+            
+        </div>
+
+        <div class="center small">
+            <em>&#169;&#44; 1997</em> <span class="vivaldi large">E</span><span class="vivaldi">ric</span><span class="vivaldi large">K</span><span class="vivaldi">och</span>&#32;/&#32; Wordman Enterprise&#32;/&#32;<em>All Rights Reserved</em>  
+        </div>
+
+        <div class="more">
+            <a href="songs.php">Return to Poems</a>
+        </div>
+
+        
+
+	</div> <!-- End page-wdth100pct div -->
+ 
+<?php
+    include ('../shared/footer.html');            
+?>

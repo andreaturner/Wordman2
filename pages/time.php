304ab3c8e92675150ceca0e77f02130ec253aecf
Paige Israel <paigeaisrael@gmail.com>
2017-09-20 08:50:09 -0700

    Started time.php
    1. Started time.php
    2. Completed intro paragraph of time.php

---
+++ pages/time.php
@@ -0,0 +1,59 @@
+<?php
+    $page_title = 'Time';
+    include ('../shared/header.html');
+?>
+
+	<div class="page-wdth100pct">
+
+		<div class="header-center-bold">
+            Time
+        </div>
+
+        <div class="songs center italic small">
+            She stood upon the sands of her own shore&#44; her spirit questioning out&#46;<br>
+            &#8220;I know not whether to laugh or cry over the passing of time&#33;&#8221;<br>
+            The quiet water gently beckoned while the night stars shimmered&#59;<br>
+            the eyes of Angels upon her&#44; and a voice whispered to her heart alone&#58;
+        </div>
+
+        <div class="songs center">
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            
+        </div>
+
+        <div class="center small">
+            <em>&#169;&#44; 1997</em> <span class="vivaldi large">E</span><span class="vivaldi">ric</span><span class="vivaldi large">K</span><span class="vivaldi">och</span>&#32;/&#32; Wordman Enterprise&#32;/&#32;<em>All Rights Reserved</em>  
+        </div>
+
+        <div class="more">
+            <a href="poems.php">Return to Poems</a>
+        </div>
+
+	</div> <!-- End page-wdth100pct div -->
+ 
+<?php
+    include ('../shared/footer.html');            
+?>
